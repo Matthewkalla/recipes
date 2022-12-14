e5f9238ad7652d2eb6e643784a9d14ec57a8daa4
Matthewkalla <mparsons077@gmail.com>
2022-12-14 07:29:45 -0800

    removed host

--- server.py
+++ server.py
@@ -3,4 +3,4 @@ from flask_app.controllers import users_controller
 from flask_app.controllers import recipes_controller
 
 if __name__ == "__main__":
-    app.run(debug=True,host='0.0.0.0') #this runs the app initialized in our init
+    app.run(debug=True) #this runs the app initialized in our init
